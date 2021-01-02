8e9d6cc3d54243012060615a921326be24a261cf
Juan <juanmvg2006@gmail.com>
2021-01-02 13:00:26 -0300

    Add avatars links

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -21,11 +21,18 @@ class ContactFactory extends Factory
      */
     public function definition()
     {
+        $avatars = [
+            'https://cdn3.iconfinder.com/data/icons/avatars-round-flat/33/avat-01-512.png',
+            'https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png',
+            'https://www.pngarts.com/files/5/User-Avatar-Transparent.png'
+        ];
+
         return [
             'name' => $this->faker->name,
             'address' => $this->faker->address,
             'phone' => $this->faker->phoneNumber,
             'about' => $this->faker->text,
+            'avatar' => $avatars[array_rand($avatars)]
         ];
     }
 }
